c3620b4fc6dd244c88ed6379f32b117000d911ce
yunko006 <peyrachethomas@gmail.com>
2021-11-06 15:46:49 +0100

    Create a custom permission to allow only author to edit/delete his posts

--- django_rest_api/blog_api/views.py
+++ django_rest_api/blog_api/views.py
@@ -1,7 +1,19 @@
 from rest_framework import generics
+from rest_framework import permissions
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import SAFE_METHODS, BasePermission, DjangoModelPermissions
 
+
+class PostUserWritePermission(BasePermission):
+    message = "Editing posts is rstricted to the author only."
+
+    def has_object_permission(self, request, view, obj):
+        
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
 
 # view qui permet de créer
 class PostList(generics.ListCreateAPIView):
@@ -10,6 +22,8 @@ class PostList(generics.ListCreateAPIView):
 
 
 # view qui permet de get et delete des items
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    # add the new permission to allow only author to edit object
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
